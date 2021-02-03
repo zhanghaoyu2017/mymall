02e6453cf0d63ec104fedb9189f532f9a6b073b8
zhanghaoyu2017 <273903932@qq.com>
2021-02-03 10:11:22 +0800

    Initial commit

---
+++ mymall-user/src/test/java/com/zhanghaoyu/mymall/user/MymallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhanghaoyu.mymall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
