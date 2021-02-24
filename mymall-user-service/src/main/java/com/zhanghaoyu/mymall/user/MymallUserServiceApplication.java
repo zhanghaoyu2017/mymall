3ae259ae4f95054eda375502ca0c90d83ecfecfc
zhanghaoyu2017 <273903932@qq.com>
2021-02-24 16:22:34 +0800

    day4 分离user-service和user-web

---
+++ mymall-user-service/src/main/java/com/zhanghaoyu/mymall/user/MymallUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zhanghaoyu.mymall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallUserServiceApplication.class, args);
+    }
+
+}
---
+++ mymall-user-web/src/main/java/com/zhanghaoyu/mymall/user/MymallUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.zhanghaoyu.mymall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallUserWebApplication.class, args);
+    }
+
+}
